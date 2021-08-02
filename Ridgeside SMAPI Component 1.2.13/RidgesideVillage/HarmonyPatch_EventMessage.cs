4f1f7b6eb067f41734e88ae74b910327ede190e1
Rafseazz <76096474+Rafseazz@users.noreply.github.com>
2021-08-02 18:03:57 +0800

    Added SMAPI component for 1.2.13

---
+++ Ridgeside SMAPI Component 1.2.13/RidgesideVillage/HarmonyPatch_EventMessage.cs
@@ -0,0 +1,50 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using StardewValley;
+
+namespace RidgesideVillage
+{
+    //Corrects the location name in the "X has begun in Y" message
+    internal static class HarmonyPatch_EventMessage
+    {
+        private static IMonitor Monitor { get; set; }
+        private static IModHelper Helper { get; set; }
+
+        internal static void ApplyPatch(Harmony harmony, IModHelper helper)
+        {
+            Helper = helper;
+
+            Log.Trace($"Applying Harmony Patch \"{nameof(ShowGlobalMessage_Prefix)}\" prefixing SDV method \"SpecialOrders.IsTimedQuest()\".");
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Game1), nameof(Game1.showGlobalMessage)),
+                prefix: new HarmonyMethod(typeof(HarmonyPatch_EventMessage), nameof(ShowGlobalMessage_Prefix))
+            );
+        }
+
+        internal static void ShowGlobalMessage_Prefix(ref string message)
+        {
+            try
+            {
+                if (message.Contains("Custom_Ridgeside_RidgesideVillage"))
+                {
+                    message = message.Replace("Custom_Ridgeside_RidgesideVillage", Helper.Translation.Get("Gathering.location"));
+
+                }
+                else if (message.Contains("Custom_Ridgeside_Ridge"))
+                {
+                    message = message.Replace("Custom_Ridgeside_Ridge", Helper.Translation.Get("EoR.location"));
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Harmony patch \"{nameof(ShowGlobalMessage_Prefix)}\" has encountered an error. \n{e.ToString()}");
+            }
+        }
+
+    }
+}
---
+++ Ridgeside SMAPI Component 1.2.13/RidgesideVillage/ModEntry.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewValley;
+using StardewModdingAPI.Events;
+
+namespace RidgesideVillage
+{
+    public class ModEntry : Mod
+    {
+        internal static IMonitor ModMonitor { get; set; }
+        internal new static IModHelper Helper { get; set; }
+
+        internal static ModConfig Config;
+
+        private ConfigMenu ConfigMenu;
+        private CustomCPTokens CustomCPTokens;
+        private Patcher Patcher;
+
+        public override void Entry(IModHelper helper)
+        {
+            ModMonitor = Monitor;
+            Helper = helper;
+
+            ConfigMenu = new ConfigMenu(this);
+            CustomCPTokens = new CustomCPTokens(this);
+            Patcher = new Patcher(this);
+
+        }
+
+
+
+
+        private void OnGameLaunched(object sender, EventArgs e)
+        {
+            
+            Config = Helper.ReadConfig<ModConfig>();
+
+            if (!Helper.ModRegistry.IsLoaded("spacechase0.JsonAssets"))
+            {
+                Log.Error("JSON Assets is not loaded! This mod *requires* JSON Assets!");
+                return;
+            }
+            Patcher.PerformPatching();
+
+            // Custom CP Token Set-up
+            CustomCPTokens.RegisterTokens();
+
+            // Generic Mod Config Menu setup
+            //ConfigMenu.RegisterMenu();
+        }
+
+        private void OnSaveLoaded(object sender, EventArgs ex)
+        {
+            try
+            {
+                Config = Helper.ReadConfig<ModConfig>();
+            }
+            catch (Exception e)
+            {
+                Log.Debug($"Failed to load config settings. Will use default settings instead. Error: {e}");
+                Config = new ModConfig();
+            }
+
+
+            //mark greenhouses as greenhouses, so trees can be planted
+            List<string> locationsNames = new List<string>() { "Custom_Ridgeside_AguarCaveTemporary", "Custom_Ridgeside_RSVGreenhouse1", "Custom_Ridgeside_RSVGreenhouse2" };
+            foreach (var name in locationsNames)
+            {
+                GameLocation location = Game1.getLocationFromName(name);
+                if (location == null)
+                {
+                    Log.Trace($"{name} is null");
+                    continue;
+                }
+                location.isGreenhouse.Value = true;
+                Log.Trace($"{name} set to greenhouse");
+            }
+        }
+
+    }
+}
