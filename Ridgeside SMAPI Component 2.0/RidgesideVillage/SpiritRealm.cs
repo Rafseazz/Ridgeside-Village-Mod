2c8adaf097200a88ac6e3915342988222f392c90
Rafseazz <76096474+Rafseazz@users.noreply.github.com>
2021-11-13 12:24:01 +0800

    lotso stuff so I can play my mod tonight yay
    SDV Beta new language support
    Jio and Daia  spouse tilesheets
    Alissa and Parents reunion dialogue typo
    Kobayashi reunion typo
    First valid file patch for language files
    .. in dialogues
    Faye introduction
    Maive at special order board typo
    Odd Jobs typos
    Ridge Forest Spawn Nerfs
    Golden Skull Coral and Entombed Rings are now dropped by Viperials (Purple Snake Head Monster)
    Sonny dialogue bug
    Louie dialogue fixes
    Lights at Paula's clinic
    Daia rain schedule fix
    Map tile fixes
    Remove debris from falls that prevents certain artifacts from spawning
    Fixed bug where Foxbloom wasn't spawning
    Foxbloom now spawn on specific tiles
    Made hike secret hint more obvious
    Holding on to certain items will affect the ridge:
    - Pale Candelabrum = Prevents ghosts and spirits
    - Everfrost Stone = Prevents wraiths
    - Ancient Music Box = Prevents Viperials
    - Village Hero Sculpture = Prevents Serpentine Beasts
    Kimpoi won't visit you at night anymore when informing about shop
    Fixed errors with special orders on Linux and MacOS users
    Fixed strings on Pika's Exotic Ingredients Special Order
    Fixed kimpoi luau dialogue
    Finally fixed up Sean's bed
    New NPCs won't be included in the winter star when FullFestivals are set to false
    Added aesthetic mailbox at the cliff
    Extend Heaps closing time from 4PM to 6PM
    LogCabinHotel2ndFloor map fixes
    Add info about relics via Raeriyala's offerins
    Water Lab shadow at bridge
    Fix silver fish bones, inked fossil, nighblack diamond
    Daia schedule error after complete Preparations quest
    Page 8 only appears after finishing story line
    Dialogue fixes at i18ns
    Hint for elven comb

--- Ridgeside SMAPI Component 2.0/RidgesideVillage/SpiritRealm.cs
+++ Ridgeside SMAPI Component 2.0/RidgesideVillage/SpiritRealm.cs
@@ -168,7 +168,42 @@ namespace RidgesideVillage
                     new Response("page2", Helper.Translation.Get("Daia.Page2")),
                     new Response("page3", Helper.Translation.Get("Daia.Page3")),
                     new Response("page4", Helper.Translation.Get("Daia.Page4")),
-                    new Response("page8", Helper.Translation.Get("Daia.LegendFishes")),
+                    new Response("cancel", Helper.Translation.Get("Daia.BookClose")),
+                };
+                var responseActions = new List<Action>
+                {
+                    delegate
+                    {
+                        Game1.activeClickableMenu = new LetterViewerMenu(Helper.Translation.Get("Daia.RelicHint1"));
+                    },
+                    delegate
+                    {
+                        Game1.activeClickableMenu = new LetterViewerMenu(Helper.Translation.Get("Daia.RelicHint2"));
+                    },
+                    delegate
+                    {
+                        Game1.activeClickableMenu = new LetterViewerMenu(Helper.Translation.Get("Daia.RelicHint3"));
+                    },
+                    delegate
+                    {
+                        ImageMenu.Open("ShowImage \"LooseSprites/RSVDaiaPage4\" 4f", Vector2.Zero);
+                    },
+                    delegate{}
+                };
+
+                Game1.activeClickableMenu = new DialogueBoxWithActions(Helper.Translation.Get("Daia.BookPages"), responses, responseActions);
+            }
+            else if (Game1.player.eventsSeen.Contains(HasUnsealedRae) && !Game1.player.eventsSeen.Contains(75160265))
+            {
+                var responses = new List<Response>
+                {
+                    new Response("page1", Helper.Translation.Get("Daia.Page1")),
+                    new Response("page2", Helper.Translation.Get("Daia.Page2")),
+                    new Response("page3", Helper.Translation.Get("Daia.Page3")),
+                    new Response("page4", Helper.Translation.Get("Daia.Page4")),
+                    new Response("page5", Helper.Translation.Get("Daia.Page5")),
+                    new Response("page6", Helper.Translation.Get("Daia.Page6")),
+                    new Response("page7", Helper.Translation.Get("Daia.Page7")),
                     new Response("cancel", Helper.Translation.Get("Daia.BookClose")),
                 };
                 var responseActions = new List<Action>
@@ -191,14 +226,22 @@ namespace RidgesideVillage
                     },
                     delegate
                     {
-                        ImageMenu.Open("ShowImage \"LooseSprites/RSVDaiaPage8\" 4f", Vector2.Zero);
+                        Game1.activeClickableMenu = new LetterViewerMenu(Helper.Translation.Get("Daia.RelicHint4"));
+                    },
+                    delegate
+                    {
+                        Game1.activeClickableMenu = new LetterViewerMenu(Helper.Translation.Get("Daia.RelicHint5"));
+                    },
+                    delegate
+                    {
+                        ImageMenu.Open("ShowImage \"LooseSprites/RSVDaiaPage7\" 4f", Vector2.Zero);
                     },
                     delegate{}
                 };
 
                 Game1.activeClickableMenu = new DialogueBoxWithActions(Helper.Translation.Get("Daia.BookPages"), responses, responseActions);
             }
-            else
+            else if (Game1.player.eventsSeen.Contains(75160265))
             {
                 var responses = new List<Response>
                 {
