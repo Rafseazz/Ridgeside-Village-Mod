5d7fa6f515106d3741f26ef1efd2dfb8d1d3d4ed
shekurika <shekurika@gmail.com>
2021-12-16 18:00:48 +0100

    add condition for NPC Loc Patch

--- Ridgeside SMAPI Component 2.0/RidgesideVillage/HarmonyPatch_EventDetection.cs
+++ Ridgeside SMAPI Component 2.0/RidgesideVillage/HarmonyPatch_EventDetection.cs
@@ -47,21 +47,24 @@ namespace RidgesideVillage
               original: AccessTools.Constructor(typeof(MapPage), new Type[] { typeof(int), typeof(int), typeof(int), typeof(int) }),
               postfix: new HarmonyMethod(typeof(HarmonyPatch_EventDetection), nameof(MapPage_Constructor_Postfix))
             );
-
-            try
+            if (Helper.ModRegistry.IsLoaded("Bouhm.NPCMapLocations"))
             {
-                Type ModMapPageClass = Type.GetType("NPCMapLocations.Framework.Menus.ModMapPage, NPCMapLocations");
-                harmony.Patch(
-                    original: AccessTools.Method(ModMapPageClass, "draw", new Type[] { typeof(SpriteBatch) }),
-                    postfix: new HarmonyMethod(typeof(HarmonyPatch_EventDetection), nameof(MapPage_draw_Postfix))
-                );
+                try
+                {
+                    Type ModMapPageClass = Type.GetType("NPCMapLocations.Framework.Menus.ModMapPage, NPCMapLocations");
+                    harmony.Patch(
+                        original: AccessTools.Method(ModMapPageClass, "draw", new Type[] { typeof(SpriteBatch) }),
+                        postfix: new HarmonyMethod(typeof(HarmonyPatch_EventDetection), nameof(MapPage_draw_Postfix))
+                    );
+                }
+                catch (Exception e)
+                {
+                    Log.Info("Failed patching NPC Map Locations. RSV Button will probably be invisible on the map");
+                    Log.Info(e.StackTrace);
+                    Log.Info(e.Message);
+                }
             }
-            catch(Exception e)
-            {
-                Log.Warn("Couldnt patch NPC Map Locations. RSV Button will be invisible on the map");
-                Log.Warn(e.StackTrace);
-                Log.Warn(e.Message);
-            }
+           
 
 
             Vector2 topLeft = Utility.getTopLeftPositionForCenteringOnScreen(1200, 720);
