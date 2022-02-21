d1282db64ad475064f87bea5eba5122eadc7e648
Clare <ccathanlon@gmail.com>
2022-02-21 01:14:15 -0500

    redid summit house stuff but it still doesn't work :c

--- Ridgeside SMAPI Component 2.0/RidgesideVillage/SummitHouse.cs
+++ Ridgeside SMAPI Component 2.0/RidgesideVillage/SummitHouse.cs
@@ -12,7 +12,7 @@ using StardewModdingAPI.Events;
 using Microsoft.Xna.Framework;
 using StardewValley.Menus;
 using StardewModdingAPI.Utilities;
-using Netcode;
+using xTile.Tiles;
 
 namespace RidgesideVillage
 {
@@ -21,76 +21,135 @@ namespace RidgesideVillage
 
         static IModHelper Helper;
         static IMonitor Monitor;
+        static Vector2 fridge_pos = new(19, 4);
 
-        [XmlElement("fridge")]
-        public static readonly NetRef<Chest> fridge = new NetRef<Chest>(new Chest(playerChest: true));
+        // Got an NRE at this line
+        static Chest fridge = new(playerChest: true, tileLocation: fridge_pos, parentSheetIndex: 216);
         internal static void Initialize(IMod ModInstance)
         {
 
             Helper = ModInstance.Helper;
             Monitor = ModInstance.Monitor;
 
-            TileActionHandler.RegisterTileAction("RSVFridge", HandleFridge);
-            TileActionHandler.RegisterTileAction("RSVKitchen", HandleKitchen);
+            Helper.Events.Input.ButtonPressed += OnButtonPressed;
         }
 
-        private static void HandleKitchen(string tileActionString, Vector2 position)
+        private static void OnButtonPressed(object sender, ButtonPressedEventArgs e)
         {
-            /*
-            List<NetMutex> muticies = new();
-            List<Chest> mini_fridges = new List<Chest>();
-            foreach (StardewValley.Object item in GameLocation.objects.Values.Get())
+            // In-game interactions
+            if (!Game1.game1.IsActive || Game1.currentLocation == null || !Context.IsWorldReady)
+                return;
+
+            // World interactions
+            if (!Context.CanPlayerMove)
+                return;
+
+            if (e.Button.IsActionButton())
             {
-                if (item != null && (bool)item.bigCraftable && item is Chest && item.ParentSheetIndex == 216)
+                // Tile actions
+                string property = GetSingleProperty(e);
+                if (property != null)
                 {
-                    mini_fridges.Add(item as Chest);
-                    muticies.Add((item as Chest).mutex);
+                    if ((Game1.currentLocation.Name is "Custom_Ridgeside_RSVSummitHouse") && (property == "RSVKitchen"))
+                    {
+                        Helper.Input.Suppress(e.Button);
+                        UseKitchen(tilePosition: fridge_pos, fridge: fridge);
+                        return;
+                    }
+
+                    // Open Community Centre fridge door
+                    if (property == "RSVFridge")
+                    {
+                        // Open the fridge as a chest
+                        OpenFridge(fridge: fridge, isOpening: true, isUsingChest: true, button: e.Button);
+
+                        return;
+                    }
                 }
             }
-            if (fridge != null && fridge.Value.mutex.IsLocked())
+        }
+
+        public static void UseKitchen(Chest fridge, Vector2 tilePosition)
+        {
+            // Try opening a cooking menu
+            // Minifridges are ignored as they are only usable with Esca's Modding Plugins
+            if (fridge != null && fridge.mutex.IsLocked())
             {
                 Game1.showRedMessage(Game1.content.LoadString("Strings\\UI:Kitchen_InUse"));
                 return;
             }
-            MultipleMutexRequest multiple_mutex_request = null;
-            multiple_mutex_request = new MultipleMutexRequest(muticies, delegate
-            {
-                fridge.Value.mutex.RequestLock(delegate
+            fridge?.mutex.RequestLock(
+                acquired: delegate
                 {
-                    List<Chest> list = new List<Chest>();
-                    if (fridge != null)
+                    // Fridge door visuals
+                    //TrySetFridgeDoor(isOpening: true, isUsingChest: false);
+
+                    // Set new crafting page
+                    Point dimensions = new(
+                        x: 800 + IClickableMenu.borderWidth * 2,
+                        y: 600 + IClickableMenu.borderWidth * 2);
+                    Point position = Utility.Vector2ToPoint(Utility.getTopLeftPositionForCenteringOnScreen(
+                        width: dimensions.X,
+                        height: dimensions.Y));
+                    IClickableMenu menu = null;
+                    menu = new CraftingPage(
+                        x: position.X,
+                        y: position.Y,
+                        width: dimensions.X,
+                        height: dimensions.Y,
+                        cooking: true,
+                        standalone_menu: true,
+                        material_containers: new List<Chest> { fridge })
                     {
-                        list.Add(fridge);
-                    }
-                    list.AddRange(mini_fridges);
-                    Vector2 topLeftPositionForCenteringOnScreen = Utility.getTopLeftPositionForCenteringOnScreen(800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2);
-                    Game1.activeClickableMenu = new CraftingPage((int)topLeftPositionForCenteringOnScreen.X, (int)topLeftPositionForCenteringOnScreen.Y, 800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2, cooking: true, standalone_menu: true, list);
-                    Game1.activeClickableMenu.exitFunction = delegate
-                    {
-                        fridge.Value.mutex.ReleaseLock();
-                        multiple_mutex_request.ReleaseLocks();
+                        exitFunction = delegate
+                        {
+                            fridge.mutex.ReleaseLock();
+                        }
                     };
-                }, delegate
+                    Game1.activeClickableMenu = menu;
+                },
+                failed: delegate
                 {
                     Game1.showRedMessage(Game1.content.LoadString("Strings\\UI:Kitchen_InUse"));
-                    multiple_mutex_request.ReleaseLocks();
                 });
-            }, delegate
-            {
-                Game1.showRedMessage(Game1.content.LoadString("Strings\\UI:Kitchen_InUse"));
-            });
-            */
         }
 
-        private static void HandleFridge(string tileActionString, Vector2 position)
+        private static bool OpenFridge(Chest fridge, bool isOpening, bool isUsingChest, SButton? button = null)
         {
-            /*
-            Chest chest = fridge.ElementAt(0);
-            Game1.activeClickableMenu = new ItemGrabMenu(chest.GetItemsForPlayer(Game1.player.UniqueMultiplayerID), reverseGrab: false, showReceivingMenu: true, InventoryMenu.highlightAllItems, chest.grabItemFromInventory, null, chest.grabItemFromChest, snapToBottom: false, canBeExitedWithKey: true, playRightClickSound: true, allowRightClick: true, showOrganizeButton: true, 1, chest, -1, chest);
-            */
+            if (button != null)
+            {
+                Helper.Input.Suppress(button.Value);
             }
 
-    }
+            Point tilePosition = Utility.Vector2ToPoint(fridge_pos);
+            (xTile.Dimensions.Location tileA, xTile.Dimensions.Location tileB) = (
+                new(tilePosition.X, tilePosition.Y - 1),
+                new(tilePosition.X, tilePosition.Y));
+            if (fridge_pos != Vector2.Zero)
+            {
+                if (!isOpening)
+                {
 
-  
+                    fridge.mutex.ReleaseLock();
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public static string GetSingleProperty(ButtonPressedEventArgs e)
+        {
+            string[] split = Game1.currentLocation.doesTileHaveProperty((int)e.Cursor.GrabTile.X, (int)e.Cursor.GrabTile.Y, "Action", "Buildings").Split(' ');
+            if (split[0].Equals("RSVKitchen"))
+            {
+                return "RSVKitchen";
+            }
+            if (split[0].Equals("RSVFridge"))
+            {
+                return "RSVFridge";
+            }
+            return null;
+        }
+
+    }
 }
