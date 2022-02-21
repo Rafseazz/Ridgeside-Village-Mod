b5ba9e92f7a98c74f64b9ff5d8c87845df1969f7
Clare <ccathanlon@gmail.com>
2022-02-21 15:21:26 -0500

    Refactored Summit House stuff to use Tile Actions

--- Ridgeside SMAPI Component 2.0/RidgesideVillage/SummitHouse.cs
+++ Ridgeside SMAPI Component 2.0/RidgesideVillage/SummitHouse.cs
@@ -25,51 +25,18 @@ namespace RidgesideVillage
 
         // Got an NRE at this line
         static Chest fridge = new(playerChest: true, tileLocation: fridge_pos, parentSheetIndex: 216);
+
         internal static void Initialize(IMod ModInstance)
         {
 
             Helper = ModInstance.Helper;
             Monitor = ModInstance.Monitor;
 
-            Helper.Events.Input.ButtonPressed += OnButtonPressed;
+            TileActionHandler.RegisterTileAction("RSVKitchen", HandleKitchen);
+            TileActionHandler.RegisterTileAction("RSVFridge", HandleFridge);
         }
 
-        private static void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
-            // In-game interactions
-            if (!Game1.game1.IsActive || Game1.currentLocation == null || !Context.IsWorldReady)
-                return;
-
-            // World interactions
-            if (!Context.CanPlayerMove)
-                return;
-
-            if (e.Button.IsActionButton())
-            {
-                // Tile actions
-                string property = GetSingleProperty(e);
-                if (property != null)
-                {
-                    if ((Game1.currentLocation.Name is "Custom_Ridgeside_RSVSummitHouse") && (property == "RSVKitchen"))
-                    {
-                        Helper.Input.Suppress(e.Button);
-                        UseKitchen(tilePosition: fridge_pos, fridge: fridge);
-                        return;
-                    }
-
-                    // Open Community Centre fridge door
-                    if (property == "RSVFridge")
-                    {
-                        // Open the fridge as a chest
-                        OpenFridge(fridge: fridge, isOpening: true, isUsingChest: true, button: e.Button);
-
-                        return;
-                    }
-                }
-            }
-        }
-
-        public static void UseKitchen(Chest fridge, Vector2 tilePosition)
+        public static void HandleKitchen(string tileActionString, Vector2 tile_pos)
         {
             // Try opening a cooking menu
             // Minifridges are ignored as they are only usable with Esca's Modding Plugins
@@ -114,41 +81,12 @@ namespace RidgesideVillage
                 });
         }
 
-        private static bool OpenFridge(Chest fridge, bool isOpening, bool isUsingChest, SButton? button = null)
+        private static void HandleFridge(string tileActionString, Vector2 position)
         {
-            if (button != null)
+            if (position == fridge_pos)
             {
-                Helper.Input.Suppress(button.Value);
+                fridge.mutex.ReleaseLock();
             }
-
-            Point tilePosition = Utility.Vector2ToPoint(fridge_pos);
-            (xTile.Dimensions.Location tileA, xTile.Dimensions.Location tileB) = (
-                new(tilePosition.X, tilePosition.Y - 1),
-                new(tilePosition.X, tilePosition.Y));
-            if (fridge_pos != Vector2.Zero)
-            {
-                if (!isOpening)
-                {
-
-                    fridge.mutex.ReleaseLock();
-                }
-                return true;
-            }
-            return false;
-        }
-
-        public static string GetSingleProperty(ButtonPressedEventArgs e)
-        {
-            string[] split = Game1.currentLocation.doesTileHaveProperty((int)e.Cursor.GrabTile.X, (int)e.Cursor.GrabTile.Y, "Action", "Buildings").Split(' ');
-            if (split[0].Equals("RSVKitchen"))
-            {
-                return "RSVKitchen";
-            }
-            if (split[0].Equals("RSVFridge"))
-            {
-                return "RSVFridge";
-            }
-            return null;
         }
 
     }
