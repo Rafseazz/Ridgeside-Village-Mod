af078c9cc8441fd64100081ba2ef7b06aa8bc531
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-06-14 22:01:29 -0400

    Update AssetManager.cs
    events are string, string, I forgot

--- Ridgeside SMAPI Component 2.0/RidgesideVillage/AssetManager.cs
+++ Ridgeside SMAPI Component 2.0/RidgesideVillage/AssetManager.cs
@@ -10,11 +10,10 @@ namespace RidgesideVillage
         internal static void LoadEmptyJsons(AssetRequestedEventArgs e)
         {
             // Load in files for events
-#warning - fix in Stardew 1.6 for string event IDs.
             if (e.NameWithoutLocale.IsEquivalentTo("Data/Events/AdventureGuild")
                 || e.NameWithoutLocale.BaseName.StartsWith(CUSTOM_EVENTS))
             {
-                e.LoadFrom(() => new Dictionary<int, string>(), AssetLoadPriority.Low);
+                e.LoadFrom(() => new Dictionary<string, string>(), AssetLoadPriority.Low);
             }
             else if (e.NameWithoutLocale.IsEquivalentTo("Data/CustomWeddingGuestPositions"))
             { // Zero clue what this is, is this fully implemented?
