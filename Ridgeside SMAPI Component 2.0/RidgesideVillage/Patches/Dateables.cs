6c13921ceb68a980f6a9a48a2095244627c8e626
Clare <ccathanlon@gmail.com>
2022-08-19 21:15:04 -0400

    fixed anton date condition, made more special orders repeatable, and made unavailable tag go away when unlockable candidate remains platonic

--- Ridgeside SMAPI Component 2.0/RidgesideVillage/Patches/Dateables.cs
+++ Ridgeside SMAPI Component 2.0/RidgesideVillage/Patches/Dateables.cs
@@ -19,7 +19,11 @@ namespace RidgesideVillage
     internal static class Dateables
     {
         private static IModHelper Helper { get; set; }
-        private static string[] names = { "Anton", "Paula" };
+        private static Dictionary<string, string> unlockables = new Dictionary<string, string>(){
+         // Character, 8 heart event ID/response ID for becoming datable
+            { "Anton", "75160304/75163042" },
+            { "Paula", "75160352/75163521" },
+        };
 
         internal static void ApplyPatch(Harmony harmony, IModHelper helper)
         {
@@ -58,8 +62,12 @@ namespace RidgesideVillage
         private static void SocialPage_drawNPCSlot_Postfix(SocialPage __instance, SpriteBatch b, int i)
         {
             string name = __instance.names[i] as string;
-            if (names.Contains(name) && !SocialPage.isDatable(name))
+            if (unlockables.Keys.Contains(name) && !SocialPage.isDatable(name))
             {
+                int event_id = int.Parse(unlockables[name].Split("/")[0]);
+                int romance_id = int.Parse(unlockables[name].Split("/")[1]);
+                if (Game1.player.eventsSeen.Contains(event_id) && !Game1.player.dialogueQuestionsAnswered.Contains(romance_id))
+                    return;
                 int width = (IClickableMenu.borderWidth * 3 + 128 - 40 + 192) / 2;
                 string text = Game1.parseText(Helper.Translation.Get("RelationshipStatus.Locked"), Game1.smallFont, width);
                 Vector2 textSize = Game1.smallFont.MeasureString(text);
@@ -68,63 +76,6 @@ namespace RidgesideVillage
                 b.DrawString(Game1.smallFont, text, new Vector2((__instance.xPositionOnScreen + 192 + 8) - textSize.X / 2f, sprites[i].bounds.Bottom - (textSize.Y - lineHeight)), Game1.textColor);
             }
         }
-
-        /*
-        public static class TextReplacementPatch
-        {
-            public static bool IsComplicated(bool datable, string name)
-            {
-                //Log.Debug("RSV: Entered IsComplicated");
-                //Log.Debug($"RSV: NPC {name} is datable: {datable}");
-                if (datable)
-                    return true; 
-                return names.Contains(name);
-            }
-
-            public static string ReplaceText(string text, string name)
-            {
-                Log.Debug("RSV: Entered ReplaceText");
-                Log.Debug($"RSV: NPC {name} is currently {text}");
-                if (names.Contains(name) && !SocialPage.isDatable(name))
-                {
-                    text = "(complicated)";
-                }
-                Log.Debug($"RSV: NPC {name} is NOW {text}");
-                return text;
-            }
-
-            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> insns, ILGenerator ilgen)
-            {
-                List<CodeInstruction> l = insns.ToList();
-
-                for (int i = 0; i < l.Count; i++)
-                {
-                    CodeInstruction current = l[i];
-                    CodeInstruction next = l[i + 1];
-                    if (current.Calls(AccessTools.Method(typeof(SocialPage), nameof(SocialPage.isDatable))))
-                    {
-                        //Log.Debug($"RSV: Found isDatable call");
-                        l.InsertRange(i + 1, new CodeInstruction[] {
-                            new CodeInstruction(OpCodes.Ldloc_0), // this should load the second argument string name
-                            new CodeInstruction(OpCodes.Call, typeof(TextReplacementPatch).GetMethod("IsComplicated"))
-                        });
-                    }
-                    else if (current.opcode == OpCodes.Stloc_S && next.opcode == OpCodes.Ldsfld && next.operand.ToString().Contains("borderWidth"))
-                    {
-                        Log.Debug($"RSV: Found end of if block");
-                        l.InsertRange(i - 1, new CodeInstruction[] {
-                            //new CodeInstruction(OpCodes.Stloc_S, l[i+1].operand), // this should load the first argument string text
-                            new CodeInstruction(OpCodes.Ldloc_0), // this should load the second argument string name
-                            new CodeInstruction(OpCodes.Call, typeof(TextReplacementPatch).GetMethod("ReplaceText"))
-                        });
-                        break;
-                    };
-                    Log.Debug($"RSV: {current}");
-                }
-                return l.AsEnumerable();
-            }
-        }
-        */
                 
     }
 }
