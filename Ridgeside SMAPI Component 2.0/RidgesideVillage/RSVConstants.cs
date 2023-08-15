3d7fe023dc558d8d54fc6574acee4f5c0d836c5c
Clare <ccathanlon@gmail.com>
2023-08-15 00:21:22 -0400

    cp tokens for bryle and zayne's schedules

--- Ridgeside SMAPI Component 2.0/RidgesideVillage/RSVConstants.cs
+++ Ridgeside SMAPI Component 2.0/RidgesideVillage/RSVConstants.cs
@@ -32,8 +32,11 @@ namespace RidgesideVillage
         public const int E_GRANDMA = 75160383;
         public const int E_SUMMITUNLOCK = 75160387;
         public const int E_BUSSTOP_INTRO = 75160186;
-        public const int E_IRENE_NODECAY = 75160324;
-        public const int E_ZAYNE_NODECAY = 75160338;
+        public const int E_ZAYNE_INTRO = 75160338;
+        public const int E_ZAYNE_2H = 75160436;
+        public const int E_ZAYNE_6H = 75160438;
+        public const int E_BRYLE_INTRO = 75160375;
+        public const int E_BRYLE_8H = 75160453;
 
         // Quests
         public const int Q_CURSEDGH1 = 72860001;
