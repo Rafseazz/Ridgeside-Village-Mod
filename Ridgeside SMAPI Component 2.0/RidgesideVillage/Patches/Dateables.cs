322a7f5ff26aac1b5b8d53d94fb9460d7ab48693
Rafseazz <76096474+Rafseazz@users.noreply.github.com>
2023-08-15 23:02:30 +0800

    fixed Irene bug

--- Ridgeside SMAPI Component 2.0/RidgesideVillage/Patches/Dateables.cs
+++ Ridgeside SMAPI Component 2.0/RidgesideVillage/Patches/Dateables.cs
@@ -55,6 +55,7 @@ namespace RidgesideVillage
 
             Helper.Events.Multiplayer.ModMessageReceived += OnMessageReceived;
             Helper.Events.GameLoop.DayEnding += OnDayEnding;
+            Helper.Events.GameLoop.DayStarted += OnDayStarted;
             Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
             SpaceEvents.OnEventFinished += OnEventFinished;
         }
