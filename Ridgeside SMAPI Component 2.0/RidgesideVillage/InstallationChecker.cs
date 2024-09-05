edd654484c03f9776914d073abadb6188876a0c9
Clare <ccathanlon@gmail.com>
2024-09-05 17:31:18 -0400

    removed Custom Gift Dialogue utility stuff and converted it to 1.6. also debugging

--- Ridgeside SMAPI Component 2.0/RidgesideVillage/InstallationChecker.cs
+++ Ridgeside SMAPI Component 2.0/RidgesideVillage/InstallationChecker.cs
@@ -60,7 +60,7 @@ namespace RidgesideVillage
                 }
                 else if (dependency.name == "RSV Extra Music Add-on")
                 {
-                    Log.Trace($"RSV's extra music add-on is no longer needed in this version of the main mod. You may delete the music add-on mod.");
+                    Log.Warn($"RSV's extra music add-on is no longer needed in this version of the main mod. You may delete the music add-on mod.");
                 }
                 else if (dependency.required && !helper.ModRegistry.IsLoaded(dependency.uniqueID))
                 {
